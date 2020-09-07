d3ef11e26ae95d22b7a1780f127eeae5cf8f8e57
Ge Yang <yangge1987@gmail.com>
2020-09-07 00:44:49 -0700

    add working code

---
+++ alice/server.py
@@ -0,0 +1,38 @@
+import asyncio
+import yaml
+
+block_size = 64
+
+
+async def handle_echo(reader, writer):
+    header_str = None
+    while True:
+        data = await reader.read(block_size)
+        if data == b'':
+            break
+        if header_str is None:
+            *header_str, data = data.split(b'\n---\n')
+            if header_str:
+                print("header >>>", header_str)
+                header = yaml.load(b''.join(header_str).decode('utf-8'), Loader=yaml.FullLoader)
+                print(header)
+
+        message = data.decode()
+        # addr = writer.get_extra_info('peername')
+        print(message, end="")
+
+    print("Closed the connection")
+    writer.close()
+
+
+async def main():
+    server = await asyncio.start_server(handle_echo, '127.0.0.1', 8888)
+
+    addr = server.sockets[0].getsockname()
+    print(f'Serving on {addr}')
+
+    async with server:
+        await server.serve_forever()
+
+
+asyncio.run(main())
